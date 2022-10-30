71984b0365e62d5d9dce51efeb92ecbf0f861113
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2022-10-30 18:53:29 +0530

    updated

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -22,5 +22,43 @@ function Menu() {
     );
 }
 
+// async function getMenu() {
+//     let response = await fetch(menu.json);
+//     let menu = await response.json();
+//     let n = 1
+//     return menu.map(menu => {
+//         menu.id = n
+//         n += 1
+//         return menu
+//     })
+// }
+
+// function getMenuHtml(menu) {
+//     return 
+//                `<h4>Recommended (20)</h4>
+//                <div className="menu">
+//                    <div className="menu-name">
+//                        <img src="${menu.veg-symbol}" alt="veg symbol" />
+//                        <p>${menu.item-name}</p>
+//                        <img className="star" src="${manu.star-rate}" alt="Star Rating" />
+//                        <p>${menu.amount}</p>
+//                    </div>
+//                    <div className="menu-pic">
+//                        <img className="food-pic" src="${menu.food-img}" alt="food pic" />
+//                        <button className="add-btn">${menu.button}</button>
+//                    </div>
+//                </div>
+//            `
+// }
+
+// function displayMenu(menu) {
+//     document.body.innerHTML = `<div class="my-menu">
+//         ${menu.map(getMenuHtml).join('')}
+//     </div>`
+// }
+
+// getMenu() 
+//     .then(displayMenu)
+//     .catch(e => console.log(e))
 
 export default Menu
