2706d07b566acbcb7be08fa99bc862b4cee3e122
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2022-10-31 13:43:44 +0530

    updated

--- src/components/view cart/view.jsx
+++ src/components/view cart/view.jsx
@@ -7,9 +7,17 @@ function ViewCart() {
             <div className="view-btn">
                 <button>View Cart</button>
             </div>
-            <div>
-                <img src="images/back.png" alt="back button" />
-                
+            <div className="back">
+                {/* <img src="images/back.png" alt="back button" /> */}
+                <p>  Back</p>
+                <h6>Table No: 10</h6>
+                <p>Order summary</p>
+                <div className="order-items">
+                    <img src="images/veg.png" alt="veg symbol" />
+                    <p>Veg Fried Rice</p>
+                    <input type="number" />
+                </div>
+                <button>Conform Your Order</button>
             </div>
         </div>
     );
